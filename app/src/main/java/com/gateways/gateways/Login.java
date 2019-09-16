c8275114ab0597fcba622ed51867d72fe6b15a25
Jagraj02 <jagrajs.jgi@gmail.com>
2019-09-17 00:39:38 +0530

    Logoaa

--- app/src/main/java/com/gateways/gateways/Login.java
+++ app/src/main/java/com/gateways/gateways/Login.java
@@ -43,7 +43,10 @@ public class Login extends AppCompatActivity {
         pDialog1 = new KAlertDialog(Login.this, KAlertDialog.PROGRESS_TYPE);
         if(user != null)
         {
-
+            pDialog1.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+            pDialog1.setTitleText("Loading");
+            pDialog1.setCancelable(false);
+            pDialog1.show();
             successfulLogin();
         }
         login.setOnClickListener(new View.OnClickListener() {
@@ -52,37 +55,26 @@ public class Login extends AppCompatActivity {
 
                 try {
 
-
                     emailText = email.getText().toString().trim();
                     passwordText = password.getText().toString();
-
                     pDialog1.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
                     pDialog1.setTitleText("Loading");
                     pDialog1.setCancelable(false);
                     pDialog1.show();
-
-
                     mAuth.signInWithEmailAndPassword(emailText,passwordText)
                             .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
-                                        // Sign in success, update UI with the signed-in user's information
-//                                        Toast.makeText(Login.this, "Successful", Toast.LENGTH_SHORT).show();
-                                        //Log.d("", "signInWithEmail:success");
                                         successfulLogin();
                                     } else {
                                         pDialog1.hide();
                                         final KAlertDialog pDialog2 = new KAlertDialog(Login.this, KAlertDialog.ERROR_TYPE);
-
                                         pDialog2.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
                                         pDialog2.setTitleText("Invalid Credentials");
-
                                         pDialog2.setCancelable(false);
                                         pDialog2.show();
-
                                     }
-
                                 }
                             });
                 }
